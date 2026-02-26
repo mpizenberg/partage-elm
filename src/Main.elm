a98bc738b34c22b1ee6533b52c91191e6b8ad9b4
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2026-02-26 09:37:15 +0100

    Init elm scaffold
    Signed-off-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

---
+++ src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, text)
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view _ =
+    text "Hello, Partage!"
