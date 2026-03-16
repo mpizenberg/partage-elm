bb2f7480990cad0184628bc8225051a6270f01c0
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2026-03-16 23:18:58 +0100

    Bump cache key
    Signed-off-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

--- build-sw.mjs
+++ build-sw.mjs
@@ -4,7 +4,7 @@ import { readFileSync, writeFileSync } from "node:fs";
 writeFileSync(
   "dist/sw.js",
   generateSW({
-    cacheName: "partage-v7",
+    cacheName: "partage-v8",
     precacheUrls: [
       "/",
       // Elm and JS compilation targets
