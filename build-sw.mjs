9f0b23216b8693300ff7fc3f6bc2c7eb9e905828
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2026-05-30 12:50:36 +0200

    Fix group loading page redirect on already joined group
    Signed-off-by: Matthieu Pizenberg <matthieu.pizenberg@gmail.com>

--- build-sw.mjs
+++ build-sw.mjs
@@ -4,7 +4,7 @@ import { readFileSync, writeFileSync } from "node:fs";
 writeFileSync(
   "dist/sw.js",
   generateSW({
-    cacheName: "partage-v22",
+    cacheName: "partage-v23",
     precacheUrls: [
       "/",
       // Elm and JS compilation targets
